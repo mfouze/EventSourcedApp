5a30a935473cb0979ec4d972181f52fbbb5c7d12
Mafouze Koto <mfouze_93@yahoo.fr>
2018-03-07 16:40:14 +0100

    Dépendances mises a jour

--- ToDoItem/src/main/java/fr/dataScale/api/CreateToDoItemCommand.java
+++ ToDoItem/src/main/java/fr/dataScale/api/CreateToDoItemCommand.java
@@ -1,8 +1,32 @@
 package fr.dataScale.api;
 
-import java.lang.annotation.Target;
+import org.axonframework.commandhandling.annotation.TargetAggregateIdentifier;
 
 public class CreateToDoItemCommand {
 
-	
+	@TargetAggregateIdentifier
+	private final String toDoId;
+	private final String description;
+
+
+	public CreateToDoItemCommand(String toDoId, String description) {
+		super();
+		this.toDoId = toDoId;
+		this.description = description;
+	}
+
+
+	public String getToDoId() {
+		return toDoId;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+
+
 }
